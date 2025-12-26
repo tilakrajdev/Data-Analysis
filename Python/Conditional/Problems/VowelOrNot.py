6d3f9d7aec17ed15ef213ecc9f7ec256dbd5d4eb
= <tilakrajpanchal432@gmail.com>
2025-12-26 13:25:43 +0530

    Commiting new files

---
+++ Python/Conditional/Problems/VowelOrNot.py
@@ -0,0 +1,7 @@
+# Write a Program to check whether the passed letter is a vowel or not?
+letter = input("Enter a character: ")
+
+if letter in "aeiou":
+    print("Charcter is vowel")
+else :
+    print("Character is consonent")
